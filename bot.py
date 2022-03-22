23b2d4326fad605ea3188b2967e9257d19108dc6
igor <igorantonow314@gmail.com>
2022-03-22 18:56:45 +0300

    added buttons; dirty code
    Added route buttons. Changed stop message design. Code needs refactoring

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
+from telegram.ext import CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from database import (
     get_route,
     get_stop,
@@ -44,19 +46,24 @@ def forecast_json_to_text(forecast_json, stop_id):
     '''
     Converts result of get_forecast_by_stop into human-readable form.
     '''
+    # TRANSLATION = {'bus': 'автобус', 'trolley': 'троллейбус',
+    #                'tram': 'трамвай', 'ship': 'аквабус'}
     assert forecast_json['success']
     msg = ''
+    routes = set()
     for p in forecast_json['result']:
         route_id = int(p['routeId'])
         route = get_route(route_id)
-        TRANSLATION = {'bus': 'автобус', 'trolley': 'троллейбус',
-                       'tram': 'трамвай', 'ship': 'аквабус'}
-        msg += '*' + (TRANSLATION[route.transport_type] + ' №'
-                      + route.route_short_name
-                      + '*\n_прибудет в ' + p['arrivingTime'].split()[1][:-3]
-                      + '_\n')
-        msg += '_маршрут: _' + '/route\\_' + str(route_id) + '\\_'
-        msg += str(get_direction_by_stop(stop_id, route_id)) + '\n'
+        msg += ('_' + p['arrivingTime'].split()[1][:-3] + '_.................'
+                + TRANSPORT_TYPE_EMOJI[route.transport_type]
+                + '*' + route.route_short_name.ljust(3) + '*\n')
+        routes.add(route_id)
+    # msg += '_Посмотреть маршруты:_\n'
+    # for i in routes:
+    #     msg += TRANSPORT_TYPE_EMOJI[get_route(i).transport_type]
+    #     msg += get_route(i).route_short_name.ljust(3) + '    '
+    #     msg += '/route\\_' + str(route_id) + '\\_'
+    #     msg += str(get_direction_by_stop(stop_id, route_id)) + '\n'
     return msg
 
 
@@ -75,12 +82,38 @@ def stop_info(stop_id):
         msg += 'посмотрите другие остановки._\n'
     msg += '\n'
     msg += 'Обновить: /stop\\_' + str(stop_id)
-    return msg
+    reply_markup = make_buttons_with_routes(forecast_json, stop_id)
+    return msg, reply_markup
 
 
-def send_stop_info(update: Update, context: CallbackContext):
-    stop_id = int(update.message.text.replace('/stop_', ''))
-    update.message.reply_text(stop_info(stop_id), parse_mode='markdown')
+def make_buttons_with_routes(forecast_json, stop_id):
+    routes = [int(p['routeId']) for p in forecast_json['result']]
+    routes = set(routes)
+    keyboard = []
+    l5 = []
+    for route_id in routes:
+        l5.append(InlineKeyboardButton(
+            TRANSPORT_TYPE_EMOJI[get_route(route_id).transport_type]
+            + get_route(route_id).route_short_name,
+            callback_data='/route_' + str(route_id) + '_'
+                          +str(get_direction_by_stop(stop_id, route_id))
+            ))
+        if len(l5) >= 5:
+            keyboard.append(l5)
+            l5 = []
+    if len(l5) > 0:
+        for i in range(5 - len(l5)):
+            l5.append(InlineKeyboardButton(' ', callback_data='/test'))
+    keyboard.append(l5)
+    return InlineKeyboardMarkup(keyboard)
+
+
+def send_stop_info(update: Update, context: CallbackContext, stop_id=None):
+    if not stop_id:
+        stop_id = int(update.message.text.replace('/stop_', ''))
+    msg, reply_markup = stop_info(stop_id)
+    update.message.reply_text(msg, parse_mode='markdown',
+                              reply_markup=reply_markup)
 
 
 def nevskii(update: Update, context: CallbackContext):
@@ -93,9 +126,7 @@ def nevskii(update: Update, context: CallbackContext):
 
 
 def random_stop(update: Update, context: CallbackContext):
-    msg = stop_info(get_random_stop_id())
-    context.bot.send_message(chat_id=update.effective_chat.id, text=msg,
-                             parse_mode='markdown')
+    send_stop_info(update, context, get_random_stop_id())
 
 
 def nearest_stops(update: Update, context: CallbackContext):
@@ -128,10 +159,46 @@ def route_info(route_id: int, direction: int):
 
 def send_route_info(update: Update, context: CallbackContext):
     route_id, direction = map(int, update.message.text
-                              .replace('/route_', '')
-                              .split('_'))
+                                   .replace('/route_', '')
+                                   .split('_'))
     update.message.reply_text(route_info(route_id, direction),
                               parse_mode='markdown')
+
+
+def send_routes_buttons(update: Update, context: CallbackContext): # routes: list[int], chat_id):
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data='1'),
+            InlineKeyboardButton("Option 2", callback_data='2'),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data='3')],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+    query_text = str(query.data)
+    print(query_text)
+    if query_text.startswith('/'):
+        if query_text.startswith('/route_'):
+            route_id, direction = map(int, query_text
+                                           .replace('/route_', '')
+                                           .split('_'))
+            context.bot.send_message(text=route_info(route_id, direction),
+                              chat_id=update.effective_chat.id,
+                              parse_mode='markdown')
+        if query_text == '/test':
+            context.bot.send_message(text='ok', chat_id=update.effective_chat.id)
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    # context.bot.send_message(chat_id=update.effective_chat.id, text=f"Selected option: {query.data}")
 
 
 def start_message(update: Update, context: CallbackContext):
@@ -165,6 +232,8 @@ def start_bot():
         MessageHandler(Filters.regex('/stop_([0-9])+'), send_stop_info),
         MessageHandler(Filters.regex('/route_([0-9])+_[0-1]'),
                        send_route_info),
+        CommandHandler('test', send_routes_buttons),
+        CallbackQueryHandler(button),
     ]
     for h in handlers:
         updater.dispatcher.add_handler(h)
